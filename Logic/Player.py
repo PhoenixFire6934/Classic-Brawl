b24f602222d80603c8d96b9cc16098c241161a5b
Crazor <52799759+CrazorCLB@users.noreply.github.com>
2020-12-17 18:01:25 -0500

    Reorganize player.py
    Added comment and reorganize values

--- Logic/Player.py
+++ Logic/Player.py
@@ -18,7 +18,7 @@ class Players:
 
 	settings = json.loads(content)
 
-
+	# Player data
 	high_id = 0
 	low_id = 0
 	token = None
@@ -28,28 +28,36 @@ class Players:
 	brawler_id = 0
 	skin_id = 0
 
-
+	
+	# Brawler data
 	skins_id = Skins.get_skins_id()
 	brawlers_id = Characters.get_brawlers_id()
 	card_skills_id = Cards.get_spg_id()
 	card_unlock_id = Cards.get_brawler_unlock()
-
-
 	brawler_power_level = settings['BrawlerPowerLevel']
 	brawler_trophies_for_rank = settings['BrawlerTrophiesForRank']
 	brawler_trophies = settings['BrawlerTrophies']
 	brawler_upgrade_points = settings['BrawlerUpgradePoints']
+	gadget = 255
+	starpower = 76
+	
+	
+	
+	# Player info
+	name = "Guest"
+	profileIcon = 0
+	namecolor = 0
+	DoNotDistrub = 0
+	brawlBoxes = settings['BrawlBoxTokens']
+	bigBoxes = settings['BigBoxTokens']
+	starPoints = settings['Starpoints']
 	trophies = settings['Trophies']
 	gems = settings['Gems']
 	gold = settings['Gold']
 	tickets = settings['Tickets']
-	name = "Guest"
-	profileIcon = 0
-	brawlBoxes = settings['BrawlBoxTokens']
-	bigBoxes = settings['BigBoxTokens']
-	starPoints = settings['Starpoints']
 
-
+	
+	# Server config
 	update_url = settings['UpdateUrl']
 	patch_url = settings['PatchUrl']
 	patch_sha = Fingerprint.loadFinger("GameAssets/fingerprint.json")
@@ -68,12 +76,13 @@ class Players:
 	if settings['Maintenance']:
 		err_code = 10
 		maintenance = True
-
+	
+	
+	# Chat data
 	message_tick = 0
 	bot_message_tick = 0
 
-	BattleResult = 0
-
+	# Friendly game data & info
 	brawlers_trophies = {}
 
 	for id in brawlers_id:
@@ -84,11 +93,9 @@ class Players:
 	for id in brawlers_id:
 		brawlers_skins.update({f'{id}': 0})
 
-
-	gadget = 255
-	starpower = 76
-	namecolor = 0
+	BattleResult = 0
 	GameType = 0
+	useGadget = 1
 	Rank = 0
 	Team = 0
 	Bot1 = 0
@@ -102,7 +109,6 @@ class Players:
 	Bot5 = 0
 	Bot5N = None
 	useGadget = 1
-	DoNotDistrub = 0
 
 
 
