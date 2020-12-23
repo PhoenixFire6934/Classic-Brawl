a0972c76934f0532197517f848851ea0a24cb913
Phoenix <63543285+PhoenixFire6879@users.noreply.github.com>
2020-12-23 14:48:41 +0200

    Update Player_Profile_Message.py

--- Packets/Messages/Server/Home/Player_Profile_Message.py
+++ Packets/Messages/Server/Home/Player_Profile_Message.py
@@ -11,53 +11,98 @@ class PlayerProfileMessage(Writer):
     def encode(self):
         self.writeVint(0)  # High Id
         self.writeVint(1)  # Low Id
-        self.writeVint(0)
 
-        self.writeVint(len(self.player.brawlers_id))  # brawlers array
+        self.writeBoolean(False)
+
+        self.writeVint(len(self.player.brawlers_id))  # Unlocked Brawlers Array
 
         for brawler_id in self.player.brawlers_id:
             self.writeScId(16, brawler_id)
             self.writeVint(0)
-            self.writeVint(99999)
-            self.writeVint(99999)
-            self.writeVint(10) # power lvl
+            self.writeVint(99999) # Brawler Trophies
+            self.writeVint(99999) # Brawler Highest Trophies
+            self.writeVint(10)    # Power Level
 
+        self.writeVint(15) # Count
 
-        self.writeVint(14)
         self.writeVint(1)
-        self.writeVint(99999)  # 3v3 victories
+        self.writeVint(99999)  # 3v3 Victories
+
         self.writeVint(2)
-        self.writeVint(1262469)
+        self.writeVint(1262469) # Experience
+
         self.writeVint(3)
-        self.writeVint(99999)
+        self.writeVint(self.player.trophies)  # Trophies
+
         self.writeVint(4)
-        self.writeVint(self.player.trophies)  # highest trophies
+        self.writeVint(self.player.trophies)  # Highest Trophies
+
         self.writeVint(5)
+        self.writeVint(len(self.player.brawlers_id)) # Brawlers Count
 
-        self.writeVint(len(self.player.brawlers_id))
-        self.writeVint(len(self.player.brawlers_id))
+        self.writeVint(6)
+        self.writeVint(0)
 
-        self.writeVint(28000000)
-        self.writeVint(8)
-        self.writeVint(99999)  # solo victories
-        self.writeVint(9)
-        self.writeVint(21)
-        self.writeVint(10)
+        self.writeVint(7)
         self.writeVint(99999)
+
+        self.writeVint(8)
+        self.writeVint(99999)  # Solo Victories
+
+        self.writeVint(9)
+        self.writeVint(99999)  # Best Robo Rumble Time
+
+        self.writeVint(10)
+        self.writeVint(99999)  # Best Time as Big Brawler
+
         self.writeVint(11)
-        self.writeVint(99999)  # duo victories
+        self.writeVint(99999)  # Duo Victories
+
         self.writeVint(12)
-        self.writeVint(21)
+        self.writeVint(10)     # Highest Boss Fight Lvl Passed
+
         self.writeVint(13)
         self.writeVint(99999)
+
         self.writeVint(14)
-        self.writeVint(1)
+        self.writeVint(1)      # Highest Power Play Rank
+
         self.writeVint(15)
-        self.writeVint(99999)  # most challenge wins
-        self.writeString(self.player.name)
+        self.writeVint(99999)  # Most Challenge Wins
+
+
+        self.writeString(self.player.name) # Player Name
+
         self.writeVint(100)
         self.writeVint(28000000)
         self.writeVint(43000000)
-        self.writeVint(0)
-        self.writeVint(0)
+
+        in_club = True
+
+        if in_club:
+
+            self.writeBoolean(True) # Player in Club
+
+            self.writeInt(0) # Club High Id
+            self.writeInt(1) # Club Low Id
+
+            self.writeString("Classic Brawl") # Club Name
+
+            self.writeVint(8)
+            self.writeVint(5) # Badge ID
+
+            self.writeVint(3)  # Club Type
+            self.writeVint(2)  # Club Members Count
+
+            self.writeVint(99999) # Club total trophies
+            self.writeVint(99999) # Club required trophies
+            self.writeVint(0)
+
+            self.writeVint(0)
+            self.writeVint(0)
+
+        else:
+            self.writeVint(0)
+            self.writeVint(0)
+
 
