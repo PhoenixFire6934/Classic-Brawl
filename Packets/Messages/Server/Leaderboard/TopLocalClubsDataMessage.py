674632dcd04a6714309dd5c9ffabfe15ce730dc1
Crazor <52799759+CrazorCLB@users.noreply.github.com>
2021-02-20 14:31:25 -0500

    Last batch of update

--- Packets/Messages/Server/Leaderboard/TopLocalClubsDataMessage.py
+++ Packets/Messages/Server/Leaderboard/TopLocalClubsDataMessage.py
@@ -26,23 +26,27 @@ class GetLeaderboardClubLocalOkMessage(Writer):
         self.club_data.sort(key=by_trophy, reverse=True)
 
         for club in self.club_data:
+
+        for club in self.club_data:
+            if club["clubID"] == self.player.club_low_id:
+                self.indexOfClub = self.club_data.index(club) + 1
             DataBase.loadClub(self, club['clubID'])
-            self.writeVint(0)  # Club High ID
-            self.writeVint(club['clubID'])  # Club Low ID
+            self.writeVint(0) # Club High ID
+            self.writeVint(club['clubID']) # Club Low ID
 
             self.writeVint(1)
-            self.writeVint(self.clubtrophies)  # Club Trophies
+            self.writeVint(self.clubtrophies) # Club Trophies
             self.writeVint(2)
 
-            self.writeString(self.clubName)  # Club Name
-            self.writeVint(self.clubmembercount)  # Club Members Count
+            self.writeString(self.clubName) # Club Name
+            self.writeVint(self.clubmembercount) # Club Members Count
 
-            self.writeVint(8)  # Club Badge
-            self.writeVint(self.clubbadgeID)  # Club Name Color
+            self.writeVint(8) # Club Badge
+            self.writeVint(self.clubbadgeID) # Club Name Color
 
 
         self.writeVint(0)
-        self.writeVint(0)
+        self.writeVint(self.indexOfClub)
         self.writeVint(0)
         self.writeVint(0)
 
