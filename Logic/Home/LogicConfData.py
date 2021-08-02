29e616c4ee914a786e2d5396044e411f8b6c1c7b
Phoenix <63543285+PhoenixFire6879@users.noreply.github.com>
2021-08-02 11:35:49 +0300

    Update LogicConfData.py

--- Logic/Home/LogicConfData.py
+++ Logic/Home/LogicConfData.py
@@ -42,7 +42,7 @@ class LogicConfData:
         for x in range(1):
             # IntValueEntry::encode
             self.writeInt(1)         # Unknown
-            self.writeInt(41000000)  # Theme ID
+            self.writeInt(41000000 + self.player.theme_id)  # Theme ID
 
         self.writeVInt(0)
         for x in range(0):
@@ -51,12 +51,3 @@ class LogicConfData:
             self.writeVInt(0)
             self.writeVInt(0)
             self.writeVInt(0)
-
-
-        # self.writeVInt(0)
-        # for x in range(0):
-        #     self.writeVInt(0)
-        #     self.writeVInt(0)
-        #     for x in range(3):
-        #         self.writeInt(0)
-        #         self.writeStringReference('')
