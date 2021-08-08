06f0ead95c7e75997e9b2c7fbbe943bbccb08ed3
Phoenix <63543285+PhoenixFire6879@users.noreply.github.com>
2021-08-08 12:52:24 +0300

    Update GetLeaderboardMessage.py

--- Protocol/Messages/Client/GetLeaderboardMessage.py
+++ Protocol/Messages/Client/GetLeaderboardMessage.py
@@ -18,11 +18,13 @@ class GetLeaderboardMessage(Reader):
         if  self.type == 1:
             self.player.leaderboard_type = 1
             players = db.load_all_players_sorted({}, 'Trophies')
+            players.reverse()
 
             LeaderboardMessage(self.client, self.player, players).send()
 
         elif self.type == 2:
             self.player.leaderboard_type = 2
             clubs = db.load_all_clubs_sorted({}, 'Trophies')
+            clubs.reverse()
 
             LeaderboardMessage(self.client, self.player, clubs).send()
